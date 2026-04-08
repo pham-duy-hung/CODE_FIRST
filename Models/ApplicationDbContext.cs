c4ee66334e27d190bdbd972ff35a555744dd32eb
Hungp <hungphamduy456654@gmail.com>
2026-04-08 18:39:45 +0700

    Create model

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CODE_FIRST.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        // Khai báo tập thực thể (Bảng)
+        public DbSet<BacSi> BacSis { get; set; }
+    }
+}
---
+++ Models/BacSi.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CODE_FIRST.Models
+{
+    public class BacSi
+    {
+        [Key] // Tự hiểu là Primary Key, Identity(1,1)
+        public int MaBS { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống tên")]
+        [StringLength(100)]
+        public string TenBacSi { get; set; }
+
+        public string ChuyenKhoa { get; set; }
+    }
+}
